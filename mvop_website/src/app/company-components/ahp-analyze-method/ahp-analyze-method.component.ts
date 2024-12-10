c52cb3f1a01123c8ca6256a43afb456646e21532
domenFilak <domen.filak2001@gmail.com>
2024-12-10 21:35:16 +0100

    Angular ahp method, inputs are loged into the console

--- mvop_website/src/app/company-components/ahp-analyze-method/ahp-analyze-method.component.ts
+++ mvop_website/src/app/company-components/ahp-analyze-method/ahp-analyze-method.component.ts
@@ -59,25 +59,38 @@ export class AhpAnalyzeMethodComponent implements OnInit {
     });
   }
 
-  // Function to evaluate fractions (e.g., '1/3' -> 0.333333...)
-  parseFraction(value: string): number {
-    if (value.includes('/')) {
-      const [numerator, denominator] = value.split('/').map(part => parseFloat(part.trim()));
-      return numerator / denominator;
-    }
-    return parseFloat(value); // Return as a float if not a fraction
-  }
-
   // Update the matrix with the string input from ngModel
   updateCellValue(header: string, rowIndex: number, colIndex: number, value: string) {
-    const parsedValue = this.parseFraction(value); // Parse the value to float
     // Save the string representation in the matrixValues
     this.alternativesValues[header][rowIndex][colIndex] = value;
     // Optional: you could also directly store the parsedValue in another matrix for further use
   }
 
+  convertFractionToFloat(fraction: string): number {
+    if (!fraction.includes('/')) {
+      return parseFloat(fraction); // Return the number itself if no fraction
+    }
+  
+    const [numerator, denominator] = fraction.split('/').map(Number);
+    const result = numerator / denominator;
+    return parseFloat(result.toFixed(4)); 
+  }
+
   // You can add a function to return the entire matrix with header names as the keys
   onSubmit() {
+
+    // Convert fractions in alternativesValues
+    for (const key in this.alternativesValues) {
+      this.alternativesValues[key] = this.alternativesValues[key].map(row =>
+        row.map(cell => this.convertFractionToFloat(cell).toString()) 
+      );
+    }
+
+    // Convert fractions in PCcriteriaValues
+    this.PCcriteriaValues = this.PCcriteriaValues.map(row =>
+      row.map(cell => this.convertFractionToFloat(cell).toString())
+    );
+
     console.log(this.PCcriteriaValues);
     console.log(this.alternativesValues);
   }
@@ -95,7 +108,6 @@ export class AhpAnalyzeMethodComponent implements OnInit {
     }
   };
   
-
   onSubmit() {
     this._pythonScriptService.postAhp(this.data).subscribe({
       next: res => {
